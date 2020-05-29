3958c8869ba2b63680b65833789061793cfd165f
kouta <kouta801kouta@gmail.com>
2020-05-29 05:42:00 +0000

    Delete user_controller#show and  routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,4 @@
 class UsersController < ApplicationController
-  before_action :require_login, only: [:show]
-  def show 
-    @user = User.find(params[:id])
-  end
-  
   def create 
     @user = User.new(user_params)
     if @user.save 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "toppages#index"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :users
+  resources :users, [:create]
   resources :stores
   resources :weeks do
     collection do
